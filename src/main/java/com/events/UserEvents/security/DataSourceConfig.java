739b7e517cfbcecee5b14b541dbf2c262e599fb4
test <test@tests-MBP.home>
2019-02-16 14:11:58 -0500

    pdf uploaded

--- src/main/java/com/events/UserEvents/security/DataSourceConfig.java
+++ src/main/java/com/events/UserEvents/security/DataSourceConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+/*import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
@@ -90,7 +90,7 @@ public class DataSourceConfig {
 
 
 
-
+*/
 
 
 
